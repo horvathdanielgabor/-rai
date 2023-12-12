0e33983fc5e19c2c0b9c49f107b118040d61c1ac
horvathdanielgabor <horvathdanielgabor@example.com>
2023-12-12 08:44:58 +0100

    masodik def es elif

--- proba.py
+++ proba.py
@@ -1,8 +1,16 @@
 def elso():
-  az=int(input("Kérek egy egész sz@m0t! "))
+    az=int(input("Kérek egy egész sz@m0t! "))
   
-  print(az)
+    print(az)
+
+def masodik():
+    i=0
+    while i==10:
+        i+=1
+        print(1)
 
 commit=int(input("Hanyas? "))
 if commit==1:
-  elso()
+    elso()
+elif commit==2:
+    masodik()
